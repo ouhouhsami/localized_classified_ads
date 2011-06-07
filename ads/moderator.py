dd80e2ff43ea298b639b42efc17568d67c33c7de
ouhouhsami <goldszmidt@ircam.fr>
2011-06-07 21:46:36 +0200

    some changes for email moderation

--- ads/moderator.py
+++ ads/moderator.py
@@ -7,8 +7,10 @@ from django.contrib.gis.db import models
 
 class HomeForSaleAdModerator(GenericModerator):
     fields_exclude = ['update_date', 'create_date', 'delete_date']
-    auto_approve_for_superusers = False
+    #auto_approve_for_superusers = False
     # manager_names = ['objects']
     # moderation_manager_class = ModerationObjectsManager
+    notify_moderator = True
+    notify_user = True
 
 moderation.register(HomeForSaleAd, HomeForSaleAdModerator)

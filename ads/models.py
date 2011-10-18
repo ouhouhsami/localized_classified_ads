9d1375c0752f347cc291b8eb92d453b7b87817ac
ouhouhsami <goldszmidt@ircam.fr>
2011-10-18 17:22:53 +0200

    fixe housing tax name

--- ads/models.py
+++ ads/models.py
@@ -212,7 +212,7 @@ class HomeAd(Ad):
                                           choices = ENERGY_CONSUMPTION_CHOICES, 
                                           null = True, blank = True)
     ad_valorem_tax = models.IntegerField('Taxe foncière (€)', null = True, blank = True, help_text="Montant annuel, sans espace, sans virgule")
-    housing_tax = models.IntegerField('Taxe foncière (€)', null = True, blank = True, help_text="Montant annuel, sans espace, sans virgule")
+    housing_tax = models.IntegerField('Taxe d\'habitation (€)', null = True, blank = True, help_text="Montant annuel, sans espace, sans virgule")
     maintenance_charges = models.IntegerField('Charges (€)', null = True, blank = True, help_text="Montant mensuel, sans espace, sans virgule")
     emission_of_greenhouse_gases = models.CharField("Émissions de gaz à effet de serre (kgeqCO2/m².an)", 
                                                     max_length = 1, 

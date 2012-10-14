4af5f8062e6f4839fa82d4e3091d3a43d106e69e
ouhouhsami <samuel.goldszmidt@gmail.com>
2012-10-14 22:31:57 +0200

    improve home page

--- utils/static/js/map-interaction.js
+++ utils/static/js/map-interaction.js
@@ -5,11 +5,15 @@ hold map interaction in home page
 */
 
 localizeMe = function(){
+	$("#localize").button('loading');
 	if (navigator.geolocation) {
 		navigator.geolocation.getCurrentPosition(function(position) {
+			$("#localize").button('reset');
 			var latlng = new L.LatLng(position.coords.latitude, position.coords.longitude);
 			map.setView(latlng, 13);
 		});
+	}else{
+		$("#localize").button('reset');
 	}
 };
 
@@ -19,6 +23,7 @@ $(document).ready(function() {
 	$("#address").keypress(function(e) {
 		if (e.which == 13) {
 			$("#center_map").trigger('click');
+			$(this).blur();
 			return false;
 		}
 	});
@@ -29,7 +34,9 @@ $(document).ready(function() {
 	// js geocode location to center the map on user input address
 	// use nominatim webservice w/ jsonp callback
 	$("#center_map").click(function(evt){
+		$(this).button('loading');
 		var address = document.getElementById("address").value;
+		var self = this;
 		$.ajax({
 			url: "http://nominatim.openstreetmap.org/search/"+address+' ',
 			data: {format: 'json', addressdetails:'1', limit:'1', countrycodes:'fr', polygon:'1', json_callback:'jsonpCallback'},
@@ -37,8 +44,15 @@ $(document).ready(function() {
 			jsonp: 'callback',
 			jsonpCallback: "jsonpCallback",
 			success: function(data){
-				var latlng = new L.LatLng(data[0].lat, data[0].lon);
-				map.setView(latlng, 13);
+				$(self).button('reset');
+				if(data.length > 0){
+					// result
+					var latlng = new L.LatLng(data[0].lat, data[0].lon);
+					map.setView(latlng, 13);
+				}else{
+					// no results
+					$("#address").attr('placeholder', 'adresse erronée').val('')
+				}
 				// below, to create a geo for the address search based on nominatin json
 				// console.log(L.geoJSON.coordsToLatlngs(data[0].polygonpoints))
 			}
